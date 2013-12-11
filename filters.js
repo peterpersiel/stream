0754dd548928c7289ea9e325f94934f544110d1d
Peter Persiel <persiel.peter@gmail.com>
2013-12-11 02:02:57 +0100

    init

---
+++ filters.js
@@ -0,0 +1,26 @@
+exports.twitter_date = function(tdate) {
+  var system_date = new Date(Date.parse(tdate));
+  var user_date = new Date();
+
+  var diff = Math.floor((user_date - system_date) / 1000);
+
+  if (diff <= 1) {return "just now";}
+  if (diff < 20) {return diff + " seconds ago";}
+  if (diff < 40) {return "half a minute ago";}
+  if (diff < 60) {return "less than a minute ago";}
+  if (diff <= 90) {return "one minute ago";}
+  if (diff <= 3540) {return Math.round(diff / 60) + " minutes ago";}
+  if (diff <= 5400) {return "1 hour ago";}
+  if (diff <= 86400) {return Math.round(diff / 3600) + " hours ago";}
+  if (diff <= 129600) {return "1 day ago";}
+  if (diff < 604800) {return Math.round(diff / 86400) + " days ago";}
+  if (diff <= 777600) {return "1 week ago";}
+
+  return "on " + system_date;
+};
+
+exports.twitter_text = function(text) {
+  var twitter = require("twitter-text");
+
+  return twitter.autoLink(twitter.htmlEscape(text));
+};
